efe49559381ef34d2e4e2cddd58fa976179fc405
Wilson <wilson_lou@hotmail.co.uk>
2018-07-10 08:06:46 -0600

    final

---
+++ SpotifyREST/src/main/java/com/skilldistillery/spotify/SpotifyRestApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyRestApplication.class, args);
+	}
+}
